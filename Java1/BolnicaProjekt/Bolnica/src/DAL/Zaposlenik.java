7aa176a54efe9f563730b0f7853ef148524e23b8
Dino Novosel <dnovosel@omega-software.hr>
2019-06-25 18:47:37 +0200

    ds

---
+++ Java1/BolnicaProjekt/Bolnica/src/DAL/Zaposlenik.java
@@ -0,0 +1,12 @@
+package DAL;
+
+import java.math.BigDecimal;
+
+public class Zaposlenik extends Osoba{
+    private String sifraZaposlenika;
+    private int osobaId;
+    private int radnoMjestoId;
+    private BigDecimal placa;
+    private Osoba osoba;
+    private  radnoMjesto;
+}
---
+++ Java1/BolnicaProjekt/Bolnica/src/PL/FormatPolja.java
@@ -0,0 +1,5 @@
+package PL;
+
+public enum FormatPolja {
+
+}
